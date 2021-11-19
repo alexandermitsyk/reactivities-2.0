bf4feda3ac5054b16c3bdd91c7fea47613586f9a
DESKTOP-405FG5O\Mitsyk <mitsyksandor@gmail.com>
2021-11-19 17:27:54 +0100

    initial commit

---
+++ client-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import axios from 'axios';
+
+function App() {
+    const [activities, setAxctivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then((res) => {
+            console.log(res);
+            setAxctivities(res.data);
+        });
+    }, []);
+
+    return (
+    <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+            <ul>
+                {
+                    activities.map((activity: any) => <li key={activity.id}>{ activity.title }</li>)
+                }
+            </ul>
+        </header>
+    </div>
+    );
+}
+
+export default App;
