ccb4daf05433d36d65a260d3c374e777d07ed896
DESKTOP-405FG5O\Mitsyk <mitsyksandor@gmail.com>
2021-11-20 21:14:04 +0100

    Axios

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,13 +15,14 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void
     deleteActivity: (id: string) => void;
+    submitting: boolean,
 }
 
-const ActivityDashboard = ({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) => {
+const ActivityDashboard = ({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) => {
     return (
         <Grid>
             <Grid.Column width='10'>
-               <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+               <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting} />
             </Grid.Column>
             <Grid.Column width='6'>
                 {
@@ -30,11 +31,12 @@ const ActivityDashboard = ({ activities, selectedActivity, selectActivity, cance
                             activity={selectedActivity}
                             cancelSelectActivity={cancelSelectActivity}
                             openForm={openForm}
+                            
                         />
                     )
                 }
                 {
-                    editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
+                    editMode && <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
                 }
             </Grid.Column>
         </Grid>
