a126c4994611ee7e8f2df7e9f0dc8e1fd4c3a978
DESKTOP-405FG5O\Mitsyk <mitsyksandor@gmail.com>
2022-04-21 19:26:23 +0200

    Paging, sorting and Filtering added

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -8,8 +8,14 @@ interface Props {
     profile: Profile;
 }
 
-export default observer(function ProfileCard({profile}: Props) {
-    return(
+export default observer(function ProfileCard({ profile }: Props) {
+    function truncate(str: string | undefined) {
+        if (str) {
+            return str.length > 40 ? str.substring(0, 37) + '...' : str;
+        }
+    }
+
+    return (
         <Card as={Link} to={`/profiles/${profile.username}`}>
             <Image src={profile.image || '/assets/user.png'} />
             <Card.Content>
@@ -17,7 +23,7 @@ export default observer(function ProfileCard({profile}: Props) {
                     {profile.displayName}
                 </Card.Header>
                 <Card.Description>
-                    Bio goes here
+                    {truncate(profile.bio)}
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
