74a988a38e6412995cf24f31c4e068dd6a3e7f9b
DESKTOP-405FG5O\Mitsyk <mitsyksandor@gmail.com>
2022-04-21 22:34:06 +0200

    Security header fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,7 +49,7 @@ namespace API
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com/"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com/", "blob:"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-qWUQKWiPOKk5ljSucWykeaAPH00bQ6OC3eGZKDksNNk="))
               );
 
